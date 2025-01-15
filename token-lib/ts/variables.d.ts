df148f90a752537956ee7b456422f4c652450fe1
senthanal <senthanal@users.noreply.github.com>
2025-01-15 00:53:43 +0000

    Chore: Update CSS variables

--- token-lib/ts/variables.d.ts
+++ token-lib/ts/variables.d.ts
@@ -6,3 +6,8 @@ export const GlobalPrimary900: string;
 export const GlobalPrimary800: string;
 export const GlobalPrimary700: string;
 export const GlobalPrimary600: string;
+export const GlobalPrimary500: string;
+export const GlobalPrimary400: string;
+export const GlobalPrimary300: string;
+export const GlobalPrimary200: string;
+export const GlobalPrimary100: string;
--- token-lib/ts/variables.js
+++ token-lib/ts/variables.js
@@ -5,4 +5,9 @@
 export const GlobalPrimary900 = "#eb3844";
 export const GlobalPrimary800 = "#d4323d";
 export const GlobalPrimary700 = "#bc2d36";
-export const GlobalPrimary600 = "#a52730";
+export const GlobalPrimary600 = "#8d2229";
+export const GlobalPrimary500 = "#761c22";
+export const GlobalPrimary400 = "#5e161b";
+export const GlobalPrimary300 = "#461114";
+export const GlobalPrimary200 = "#2f0b0e";
+export const GlobalPrimary100 = "#170607";

0905488cfab2c3d432683a0986e8fdd86135361d
Senthanal Sirpi Manohar <senthanal@users.noreply.github.com>
2025-02-19 16:42:54 +0100

    Convert to W3C DTCG format, read more at https://docs.tokens.studio/c… (#9)
    * Convert to W3C DTCG format, read more at https://docs.tokens.studio/convert-to-dtcg-format
    
    * Chore: Update CSS variables

--- token-lib/ts/variables.d.ts
+++ token-lib/ts/variables.d.ts
@@ -2,18 +2,18 @@
  * Do not edit directly, this file was auto-generated.
  */
 
-export const GlobalPrimary900: string;
-export const GlobalPrimary800: string;
-export const GlobalPrimary700: string;
-export const GlobalPrimary600: string;
-export const GlobalPrimary500: string;
-export const GlobalPrimary400: string;
-export const GlobalPrimary300: string;
-export const GlobalPrimary200: string;
-export const GlobalPrimary100: string;
-export const GlobalBaseFontFamily: string;
-export const GlobalHeadingFontFamily: string;
-export const GlobalMonospaceFontFamily: string;
-export const GlobalHeadingH1FontSize: string;
-export const GlobalHeadingH2FontSize: string;
+export const GlobalPrimary900: string; /** primary color 800 */
+export const GlobalPrimary800: string; /** primary color 700 */
+export const GlobalPrimary700: string; /** primary 600 color code */
+export const GlobalPrimary600: string; /** primary 500 */
+export const GlobalPrimary500: string; /** primary 400 */
+export const GlobalPrimary400: string; /** primary 300 */
+export const GlobalPrimary300: string; /** primary 200 */
+export const GlobalPrimary200: string; /** primary 100 */
+export const GlobalPrimary100: string; /** base font family */
+export const GlobalBaseFontFamily: string; /** for header elements */
+export const GlobalHeadingFontFamily: string; /** for editors */
+export const GlobalMonospaceFontFamily: string; /** heading h1 element font size */
+export const GlobalHeadingH1FontSize: string; /** heading h2 element font size */
+export const GlobalHeadingH2FontSize: string; /** heading h3 element font size */
 export const GlobalHeadingH3FontSize: string;
--- token-lib/ts/variables.js
+++ token-lib/ts/variables.js
@@ -3,17 +3,17 @@
  */
 
 export const GlobalPrimary900 = "#eb3844";
-export const GlobalPrimary800 = "#d4323d";
-export const GlobalPrimary700 = "#bc2d36";
-export const GlobalPrimary600 = "#8d2229";
-export const GlobalPrimary500 = "#761c22";
-export const GlobalPrimary400 = "#5e161b";
-export const GlobalPrimary300 = "#461114";
-export const GlobalPrimary200 = "#2f0b0e";
-export const GlobalPrimary100 = "#170607";
-export const GlobalBaseFontFamily = "Roboto, Arial, sans-serif";
-export const GlobalHeadingFontFamily = "Montserrat, Arial, sans-serif";
-export const GlobalMonospaceFontFamily = "'Courier New', Courier, monospace";
-export const GlobalHeadingH1FontSize = "32px";
-export const GlobalHeadingH2FontSize = "28px";
-export const GlobalHeadingH3FontSize = "24px";
+export const GlobalPrimary800 = "#d4323d"; // primary color 800
+export const GlobalPrimary700 = "#bc2d36"; // primary color 700
+export const GlobalPrimary600 = "#8d2229"; // primary 600 color code
+export const GlobalPrimary500 = "#761c22"; // primary 500
+export const GlobalPrimary400 = "#5e161b"; // primary 400
+export const GlobalPrimary300 = "#461114"; // primary 300
+export const GlobalPrimary200 = "#2f0b0e"; // primary 200
+export const GlobalPrimary100 = "#170607"; // primary 100
+export const GlobalBaseFontFamily = "Roboto, Arial, sans-serif"; // base font family
+export const GlobalHeadingFontFamily = "Montserrat, Arial, sans-serif"; // for header elements
+export const GlobalMonospaceFontFamily = "'Courier New', Courier, monospace"; // for editors
+export const GlobalHeadingH1FontSize = "32px"; // heading h1 element font size
+export const GlobalHeadingH2FontSize = "28px"; // heading h2 element font size
+export const GlobalHeadingH3FontSize = "24px"; // heading h3 element font size
